c45f349996e2d05a27066d87f07306d5576b154f
lynnshao <smj02@163.com>
2014-05-07 00:26:39 +0800

    change_main

--- goods/public/javascripts/main.js
+++ goods/public/javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $(".all_softs a").hover(function(){
+    $(".all_softs").hover(function(){
         $(".soft_show_all").css("display", "block");
     },function(){
         $(".soft_show_all").css("display", "none");
